4b6e833d63e522cbb21f70f3373aad9acb72cb67
Евгений Сергеев <akaevgeniy@yandex.ru>
2022-03-06 20:18:12 +0300

    Fix: local storage save state

--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -16,6 +16,11 @@ function StoryPage({ items, unixTimeConvert }) {
           Total comments:{' '}
           {items.length !== 0 && story.data.kids ? story.data.kids.length : '0'}
         </p>
+        <div>
+          {items.length !== 0 && story.data.kids
+            ? story.data.kids.map((kid) => <p key={kid}>{kid}</p>)
+            : ''}
+        </div>
       </div>
     </article>
   );
